5073259f3e94c72e2de6bfdf9d5751aea30a237d
VaneskaSousa <vaneskakaren15@gmail.com>
2018-02-19 12:28:52 -0300

    Colocando a logo vertical

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,35 +1,46 @@
 package hello;
 
+import totalcross.io.IOException;
 import totalcross.sys.Settings;
 import totalcross.ui.*;
 import totalcross.ui.dialog.MessageBox;
 import totalcross.ui.event.*;
-import totalcross.ui.font.Font;
 import totalcross.ui.gfx.Color;
+import totalcross.ui.image.Image;
+import totalcross.ui.image.ImageException;
 
 public class HelloWorld extends MainWindow {
 	private Button btnHello;
-	private Label lbl;
-	final Bar barTC;
-	
-	public HelloWorld() throws Throwable {
-		super("", BORDER_LOWERED);
-		Font f = Font.getFont(true,Font.NORMAL_SIZE+4);
-		barTC = new Bar("TotalCross Ensina");
-		barTC.canSelectTitle = false;
-		barTC.setFont(f);
-		barTC.setBackForeColors(0x0A246A,Color.WHITE);		
-		add(barTC, LEFT,0,FILL,PREFERRED);
-		
-		setBackForeColors(Color.getRGB(240, 240, 240), Color.getRGB(50, 50, 50));
+
+	public HelloWorld(){
+		super("", NO_BORDER);
+		setBackForeColors(Color.WHITE, 0x323232);
 		Settings.uiAdjustmentsBasedOnFontHeight = true;
 		setUIStyle(Settings.MATERIAL_UI);
 	}
 
 	public void initUI() {
-		add(lbl = new Label("TotalCross Ensina"), CENTER+20, TOP+650, PARENTSIZE+63, PARENTSIZE+22);
-		lbl.setFont(lbl.getFont().adjustedBy(10));
-		add(btnHello = new Button("Hello"), CENTER, CENTER+10, PARENTSIZE+38, PARENTSIZE+8);
+		
+		try {
+			
+			ImageControl ic = new ImageControl(new Image("img/logo.png"));
+			ic.scaleToFit = true;
+			ic.centerImage = true;
+			
+			Label lbWelcome = new Label("Welcome to TotalCross!");
+	        lbWelcome.setFont(lbWelcome.getFont().asBold());
+
+	        add(ic, CENTER, AFTER+60, SCREENSIZE + 50, SCREENSIZE + 40);
+	        add(lbWelcome, CENTER, AFTER);
+			
+	        btnHello = new Button("Hello");
+	        btnHello.setBackColor(0xF2F2F2);
+	        add(btnHello, CENTER, AFTER+150, PARENTSIZE+38, PARENTSIZE+8);
+		
+		} catch (IOException | ImageException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void onEvent(Event e) {
